979116e275e24efc41495919c6b3971793105847
marcushurn <marcushurn@hotmail.com>
2022-01-25 20:49:47 -0800

    updated, sized and styled

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,19 +1,51 @@
+import { Divider, Grid, styled, Typography } from "@mui/material";
 import { FunctionComponent } from "react";
 import { useAppSelector } from "../app/hooks";
 import HomeBtn from "../buttons/HomeBtn";
 import ShoppingListBtn from "../buttons/ShoppingListBtn";
 import Header from "../components/Header";
 import RecipeList from "../components/RecipeList";
-import { selectFavorites } from "../redux/userState";
+import { selectFavorites, selectNumOfFavorites, selectUserName } from "../redux/userState";
+import splashImage from '../images/marvin-binnig-0zqT55YuPn8-unsplash.jpg';
+import { Box } from "@mui/material/node_modules/@mui/system";
 
+const ImagePanel = styled(Grid)({
+  display: 'flex',
+  backgroundImage: `url(${splashImage})`,
+  opacity: '77%',
+  minHeight: '70px'
+});
+
+const TextBox = styled(Box)({
+  backgroundColor: 'white',
+  padding: '15px 10px 40px 10px'
+});
 
 const Favorites: FunctionComponent = () => {
   const recipes = useAppSelector(selectFavorites);
+  const numOfFavorites = useAppSelector(selectNumOfFavorites);
+  const userName = useAppSelector(selectUserName);
+
+  const greeting = () => (userName.endsWith('s') || userName.endsWith('S')) 
+    ? userName + "'"
+    : userName + "'s"
 
   return (
     <>
       <Header leftButton={<HomeBtn />} rightButton={<ShoppingListBtn />} />
-      <h2>My favorites</h2>
+      <ImagePanel />
+      <TextBox sx={{backgroundColor: 'white', padding: ''}}>
+        <Typography color='textPrimary' variant='h4'>{greeting()} Favorites</Typography>
+        <Divider />
+        <br />
+        <Typography color='textPrimary' variant='h6'>
+          You currently have {numOfFavorites} recipes in you favorites.
+        </Typography>
+        <Typography color='textPrimary' variant='subtitle1' sx={{paddingLeft: '10px', fontStyle: 'italic' }}>
+          Ingredients for these recipes are in your shopping list
+        </Typography>
+      </TextBox>
+
       <RecipeList recipes={recipes} />
     </>
   );
