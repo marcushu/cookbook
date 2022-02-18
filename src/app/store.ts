e9996c6dd4d482a28258ada8b5797e43f7abefe9
marcushurn <marcushurn@hotmail.com>
2022-02-17 17:43:39 -0800

    removed references to delete redux files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import userReducer from '../redux/userState';
 import searchResultsReducer from '../redux/searchResultState';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     user: userReducer,
     searchResults: searchResultsReducer
   },
