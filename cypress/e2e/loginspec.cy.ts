498071954c61a4273644f77ccb8c47794363d236
marcushurn <marcushurn@hotmail.com>
2022-06-10 16:26:48 -0700

    bump to Cypress 10

---
+++ cypress/e2e/loginspec.cy.ts
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe('Log in test', () => {
+
+  it('wont log in a non user', () => {
+      cy.visit('http://localhost:3000/');
+
+      cy.contains('Log in').click();
+
+      cy.visit('http://localhost:3000/signin');
+
+      cy.get('input[placeholder="User name"]').type('abc');
+
+      cy.get('button[id="signinbtn"]').click();
+
+      cy.contains('Login error');
+  });
+
+  it('logs in and logs out a user', () => {
+      cy.visit('http://localhost:3000/signin');
+
+      cy.get('input[placeholder="User name"]').type('cypresTesUser');
+
+      cy.get('button[id="signinbtn"]').click();
+
+      cy.contains('cypresTesUser');
+
+      // log out
+      cy.get('button[aria-label="Log out"]').click();
+
+      cy.contains('Just recipes');
+
+  });
+  
+})
